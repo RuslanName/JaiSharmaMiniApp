7a0bc8da2924f41646ff22de91261bd328cd0b8e
RuslanName <ruslangeroy@gmail.com>
2025-10-30 01:04:39 +0300

    Some fixes

--- client/src/config/constants.ts
+++ client/src/config/constants.ts
@@ -1,12 +1,8 @@
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
 export const config = {
-    API_URL: process.env.API_URL || 'http://localhost:5000',
     AUTH_TOKEN_KEY: 'auth_token',
     AUTH_USER_KEY: 'auth_user',
-    MAX_WAIT_TIME_SIGNAL: 310000,
-    MAX_READY_TIME_SIGNAL: 30000,
-    PORT: 5173,
+    API_URL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+    MAX_WAIT_TIME_SIGNAL: Number(import.meta.env.VITE_MAX_WAIT_TIME_SIGNAL) || 310000,
+    MAX_READY_TIME_SIGNAL: Number(import.meta.env.VITE_MAX_READY_TIME_SIGNAL) || 30000,
+    PORT: Number(import.meta.env.VITE_PORT) || 5173,
 };
